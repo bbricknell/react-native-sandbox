41a733483b88ddc33ec0421c663803e5aea65589
bbricknell <barry.bricknell@sky.uk>
2018-04-16 15:07:34 +0100

    Remove multi platform support from app component.

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component {
   render() {
@@ -24,7 +16,10 @@ export default class App extends Component {
           To get started, edit App.js
         </Text>
         <Text style={styles.instructions}>
-          {instructions}
+          Cmd+R to reload
+        </Text>
+        <Text style={styles.instructions}>
+          Cmd+D for dev tools
         </Text>
       </View>
     );

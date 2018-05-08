7cfe73c7a9af4075f5d5060d6e82cf560520c329
bbricknell <barry.bricknell@sky.uk>
2018-05-08 15:16:03 +0100

    Adds test for button component press.

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import { Button } from './button';
 
@@ -7,5 +8,12 @@ describe('Button Component', () => {
   test('renders button correctly', () => {
     const button = Renderer.create(<Button text="button test" clickAction={() => {}} />);
     expect(button).toMatchSnapshot();
+  });
+
+  test('calls the passed callback on press', () => {
+    const mockCallback = jest.fn();
+    const button = shallow(<Button text="button test" clickAction={mockCallback} />);
+    button.simulate('press');
+    expect(mockCallback.mock.calls.length).toBe(1);
   });
 });
